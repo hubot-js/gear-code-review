3b8ba0fe45d79a7275fca61bf8dc5bcee4e684aa
robsonbittencourt <robson.luizv@gmail.com>
2017-12-08 22:50:49 -0200

    Add aditional text on review request

--- src/handlers/code-review.js
+++ src/handlers/code-review.js
@@ -4,7 +4,7 @@ exports.handle = handle;
 
 const db = require('../../database/database');
 
-function handle(hubot, message, task) {
+function handle(hubot, message, task, params) {
   if (!hubot.isFromPrivate(message)) {
     return hubot.speak(message, getMessageForChannel(hubot));
   }
@@ -14,29 +14,30 @@ function handle(hubot, message, task) {
     message: task.options.message
   };
 
-  return reviewFor(hubot, message, review);
+  return reviewFor(hubot, message, review, params);
 }
 
 function getMessageForChannel(hubot) {
   return hubot.speech('code-review:codeReview.private').end();
 }
 
-function reviewFor(hubot, message, review) {
-  postMessage(hubot, message, review);
+function reviewFor(hubot, message, review, params) {
+  postMessage(hubot, message, review, params);
   enqueue(hubot, message, review);
 }
 
-function postMessage(hubot, message, review) {
+function postMessage(hubot, message, review, params) {
   const author = hubot.getUser(message);
 
   db.reviewChannel(hubot).then((channel) => {
     const reviewChannel = channel.channel_name ? channel.channel_name : hubot.getChannelByName('general').id;
-    hubot.speakTo(reviewChannel, getChannelMessage(hubot, review, author));
+    hubot.speakTo(reviewChannel, getChannelMessage(hubot, review, author, params));
   });
 }
 
-function getChannelMessage(hubot, review, author) {
-  return hubot.speech().channel().append(review.message).replace('auth').replace('author', author).end();
+function getChannelMessage(hubot, review, author, params) {
+  return hubot.speech().channel().append(review.message)
+    .replace('auth').replace('author', author).line().replace('reviewUrl', params[0]).end();
 }
 
 function enqueue(hubot, message, review) {
